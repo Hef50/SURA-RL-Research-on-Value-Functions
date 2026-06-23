5308a882e99fee502d190396fd6eb978645c0f9d
Haresh M <hefgames50@gmail.com>
2026-06-23 04:37:08 -0400

    MLP, model, encodings

---
+++ encodings.py
@@ -0,0 +1,22 @@
+﻿import numpy as np
+
+def encode_as_channels(maze, agent_pos, goal_pos):
+    channel_walls = maze.flatten().astype(np.float32)
+
+    agent_grid = np.zeros_like(maze, dtype = np.float32)
+    agent_grid[agent_pos[0], agent_pos[1]] = 1.0
+    channel_agent = agent_grid.flatten()
+
+    goal_grid = np.zeros_like(maze, dtype = np.float32)
+    goal_grid[goal_pos[0], goal_pos[1]] = 1.0
+    channel_goal = goal_grid.flatten()
+
+    return np.concatenate([channel_walls, channel_agent, channel_goal])
+
+def encode_as_single_array(maze, agent_pos, goal_pos):
+    
+    grid = maze.copy().astype(np.float32)
+    grid[agent_pos[0], agent_pos[1]] = 2.0
+    grid[goal_pos[0], goal_pos[1]] = 3.0
+    channel = grid.flatten()
+    return channel
---
+++ model.py
@@ -0,0 +1,19 @@
+﻿import torch
+import torch.nn as nn
+
+class MazeMLP(nn.Module):
+    def __init__(self, input_dim, hidden_dim=64):
+
+        super(MazeMLP, self).__init__()
+
+        # input encoding -> hidden layer -> 5 output (action space)
+
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_dim, 5)
+    
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        logits = self.fc2(out)
+        return logits
