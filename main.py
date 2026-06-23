7985257cff8da1ef63f1d3b6fc4f518c933967da
Haresh M <hefgames50@gmail.com>
2026-06-23 02:52:51 -0400

    starting to code

---
+++ main.py
@@ -0,0 +1,11 @@
+﻿import numpy as np
+
+def generate_maze(D, seed=None):
+    if seed is not None:
+        np.random.seed(seed)
+    
+    # starting with grid of all ones (walls)
+    maze = np.ones((D, D), dtype=int)
+
+    # start Primms at (0,0) - it shouldn't matter,maze should be possible for any starting point
+    
